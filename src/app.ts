64655c7990edefb677206e7ecb0af09cc69c25a4
farhana988 <farhana744255@gmail.com>
2025-06-20 17:05:21 +0600

    created notes crud

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,14 @@
 import express, { Application, Request, Response } from "express";
+import { model, Schema } from "mongoose";
+import { notesRoutes } from "./app/controllers/notes.controller";
 const app: Application = express();
 
+app.use(express.json());
 
+app.use("/notes", notesRoutes);
 
-
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Note App');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Note App");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-export default app
+export default app;
