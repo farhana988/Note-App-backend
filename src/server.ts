884c988f5866695519b2833abe106aa388973a4e
farhana988 <farhana744255@gmail.com>
2025-06-20 11:37:15 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+require("dotenv").config();
+let server: Server;
+
+const PORT = 5000;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ljf3d.mongodb.net/Note-App?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Connected to MongoDB Using Mongoose!!");
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
